96d0a18dda25d115cf5d8b02b694c4fce9b2a789
Matt Farncombe <matt@farncombe.co>
2020-04-17 11:58:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+beatlesArray = [];
+
 function theBeatlesPlay(musicians, instruments) {
-  beatlesArray = [];
   for (var i = 0; i < musicians.length; i++) {
    beatlesArray.push(`${musicians[i]} plays ${instrument[i]}.`)
   }
