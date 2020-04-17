e6d409cba9d991213d305156b38d3532821e55f4
Matt Farncombe <matt@farncombe.co>
2020-04-17 11:59:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function theBeatlesPlay(musicians, instruments) {
   beatlesArray = [];
   for (var i = 0; i < musicians.length; i++) {
    beatlesArray.push(`${musicians[i]} plays ${instrument[i]}.`)
+   console.log(`${musicians[i]} plays ${instrument[i]}.`);
   }
   return beatlesArray;
 }
